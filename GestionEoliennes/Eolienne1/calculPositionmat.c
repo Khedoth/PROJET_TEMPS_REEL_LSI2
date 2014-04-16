e51306fe8040593b74c92378dc67a4cb34d19b22
Jean-Michel NOKAYA <jnokaya@POUTRE.univ-rennes1.fr>
2014-04-16 18:13:08 +0200

    calculPositionmat.c

--- GestionEoliennes/Eolienne1/calculPositionmat.c
+++ GestionEoliennes/Eolienne1/calculPositionmat.c
@@ -12,8 +12,21 @@
  *
  */
 
-int calculPositionMat(){
+/*
+ * calculPositionMat() : calcule la position du mat en fonction des signaux provenant des capteurs du mât.
+ */
+
+void calculPositionMat(){
 	while(1){
-		if
+		if(rotationMatH){
+			positionEolienne[0]=(positionEolienne[0]-1)%360;
+			//semGive();
+		}
+
+		else if (rotationMatAH){
+			positionEolienne[0]=(positionEolienne[0]+1)%360;
+			//semGive();
+		}
 	}
+
 }
